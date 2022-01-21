5e327a22476e9f91f6a522ca4aad4f200533e515
Etiboubou <silwyth01@gmail.com>
2022-01-21 00:22:18 -0500

    Final touch esthétique

--- src/views/Tictactoe.view.vue
+++ src/views/Tictactoe.view.vue
@@ -89,27 +89,35 @@ game.message = `It's ${game.currentPlayer.symbol} to play`
 </script>
 
 <template>
-    <div class="container bg-amber-100">
-        <div class="bg-green-200">
-            <div
-                v-for=" box in game.gameState"
-                class="bg-blue-500 w-20 h-20 m-2 text-center text-6xl font-bold rounded hover:bg-blue-600"
-            >
-                <button
-                    @click="game.currentPlayer.symbol === 'X' ? game.play(box, playerO) : game.play(box, playerX)"
-                >{{ box.content }}</button>
+    <div class="container">
+        <div class="flex">
+            <div class="m-auto inline-grid grid-cols-3">
+                <div
+                    v-for=" box in game.gameState"
+                    class="bg-blue-500 w-20 h-20 m-2 text-center text-6xl font-bold rounded hover:bg-blue-600"
+                >
+                    <button
+                        @click="game.currentPlayer.symbol === 'X' ? game.play(box, playerO) : game.play(box, playerX)"
+                    >{{ box.content }}</button>
+                </div>
             </div>
         </div>
-        <div class="bg-neutral-600">
-            <div class="bg-red-200 mx-auto my-4 font-semibold text-xl px-5 py-3">
-                <div>{{ game.message }}</div>
+        <div>
+            <div
+                class="flex items-center justify-center space-x-12 my-4 font-semibold text-xl px-5 py-3"
+            >
                 <div>Player X : {{ playerX.point }}</div>
                 <div>Player O : {{ playerO.point }}</div>
             </div>
-            <button
-                class="mx-auto my-3 font-semibold hover:bg-purple-600 text-xl px-5 py-3 bg-purple-500 rounded-lg"
-                @click="game.reset(playerX)"
-            >Reset</button>
+            <div
+                class="flex items-center justify-center my-4 font-semibold text-xl px-5 py-3 place-self-center"
+            >{{ game.message }}</div>
+            <div class="flex items-center justify-center">
+                <button
+                    class="flex items-center justify-center my-3 font-semibold hover:bg-purple-600 text-xl px-5 py-3 bg-purple-500 rounded-lg"
+                    @click="game.reset(playerX)"
+                >Reset</button>
+            </div>
         </div>
     </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,9 @@ module.exports = {
   content: ["./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",],
   theme: {
+
     container: {
       center: true,
-      padding: "2rem",
-      extend: {},
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
