afee6e82c3c4f9f4c1ee7fe47c643a106e247686
Etiboubou <silwyth01@gmail.com>
2022-01-30 13:17:22 -0500

    src images réglé

--- src/views/Resto.view.vue
+++ src/views/Resto.view.vue
@@ -6,9 +6,36 @@ import { ref, reactive } from 'vue'
 </script>
 
 <template>
-    <div id="content">
+    <div class="container bg-slate-200">
+        <div class="flex items-center">
+            <div class="basis-1/3">
+                <h2>Homepage</h2>
+            </div>
+            <div class="basis-1/3">
+                <h2>Menu</h2>
+            </div>
+            <div class="basis-1/3">
+                <h2>Contact</h2>
+            </div>
+        </div>
+        <div class="flex bg-red-200">
+            <div class="basis-1/4 bg-slate-500">
+                <h1 class="text-xl font-bold text-center align-middle">Resto Ô p'tit creux</h1>
+            </div>
+            <div class="basis-3/4 bg-slate-900">
+                <img src="/src/assets/logo.png" />
+            </div>
+        </div>
         <div>
-            <img v-bind:src="'assets' + logo.png" />
+            <div>
+                <p>Irure consectetur elit aliquip pariatur anim sint sunt in culpa. Quis et nostrud sit excepteur do enim. Nisi in anim ea elit voluptate enim magna do. Non velit deserunt aliqua dolor laborum et. Eiusmod non aute dolore nulla. Sit incididunt proident pariatur ex veniam. Cupidatat anim elit Lorem mollit Lorem cillum non qui incididunt.</p>
+            </div>
+            <div>
+                <p>Irure consectetur elit aliquip pariatur anim sint sunt in culpa. Quis et nostrud sit excepteur do enim. Nisi in anim ea elit voluptate enim magna do. Non velit deserunt aliqua dolor laborum et. Eiusmod non aute dolore nulla. Sit incididunt proident pariatur ex veniam. Cupidatat anim elit Lorem mollit Lorem cillum non qui incididunt.</p>
+            </div>
+            <div>
+                <p>Irure consectetur elit aliquip pariatur anim sint sunt in culpa. Quis et nostrud sit excepteur do enim. Nisi in anim ea elit voluptate enim magna do. Non velit deserunt aliqua dolor laborum et. Eiusmod non aute dolore nulla. Sit incididunt proident pariatur ex veniam. Cupidatat anim elit Lorem mollit Lorem cillum non qui incididunt.</p>
+            </div>
         </div>
     </div>
 </template>
