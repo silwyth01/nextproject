32d906fffd552b7dd80c82aafef7f051e5f9b601
Etiboubou <silwyth01@gmail.com>
2022-01-31 20:06:40 -0500

    mongodb added

--- src/views/Resto.view.vue
+++ src/views/Resto.view.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, reactive } from 'vue'
-
+const show = ref("Home");
 
 
 </script>
@@ -9,13 +9,13 @@ import { ref, reactive } from 'vue'
     <div class="container bg-slate-200">
         <div class="flex items-center">
             <div class="basis-1/3">
-                <h2>Homepage</h2>
+                <button type="button" @click="show = 'Home'">Homepage</button>
             </div>
             <div class="basis-1/3">
-                <h2>Menu</h2>
+                <button type="button" @click="show = 'Menu'">Menu</button>
             </div>
             <div class="basis-1/3">
-                <h2>Contact</h2>
+                <button type="button" @click="show = 'Contact'">Contact</button>
             </div>
         </div>
         <div class="flex bg-red-200">
@@ -26,7 +26,7 @@ import { ref, reactive } from 'vue'
                 <img src="/src/assets/logo.png" />
             </div>
         </div>
-        <div>
+        <div v-show="show === 'Home'">
             <div>
                 <p>Irure consectetur elit aliquip pariatur anim sint sunt in culpa. Quis et nostrud sit excepteur do enim. Nisi in anim ea elit voluptate enim magna do. Non velit deserunt aliqua dolor laborum et. Eiusmod non aute dolore nulla. Sit incididunt proident pariatur ex veniam. Cupidatat anim elit Lorem mollit Lorem cillum non qui incididunt.</p>
             </div>
@@ -35,6 +35,28 @@ import { ref, reactive } from 'vue'
             </div>
             <div>
                 <p>Irure consectetur elit aliquip pariatur anim sint sunt in culpa. Quis et nostrud sit excepteur do enim. Nisi in anim ea elit voluptate enim magna do. Non velit deserunt aliqua dolor laborum et. Eiusmod non aute dolore nulla. Sit incididunt proident pariatur ex veniam. Cupidatat anim elit Lorem mollit Lorem cillum non qui incididunt.</p>
+            </div>
+        </div>
+        <div v-show="show === 'Menu'">
+            <div>
+                <p>Petite Poutine</p>
+            </div>
+            <div>
+                <p>Moyenne Poutine</p>
+            </div>
+            <div>
+                <p>Esti d'grosse Poutine</p>
+            </div>
+        </div>
+        <div v-show="show === 'Contact'">
+            <div>
+                <p>Jean-Pierre</p>
+            </div>
+            <div>
+                <p>Paul</p>
+            </div>
+            <div>
+                <p>Le reste</p>
             </div>
         </div>
     </div>
